7af5f8a7b42b2d34ba8cc7895ecc26625e503dd4
narmada099 <88979206+narmada099@users.noreply.github.com>
2026-04-11 16:29:15 +0530

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        if(s== null || s.length()==0){
+            return false;
+        }
+        
+       s= s.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
+       int l=0;
+       int r= s.length()-1;
+       while(l<=r){
+        if(s.charAt(l)!=s.charAt(r)){
+            return false;
+        }l++;
+        r--;
+       }
+       return true;
+    }
+}
